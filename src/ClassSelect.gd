4e9c659e11d8300cc5af1bb004c1e168d7033cd3
Thallium204 <12djorden@gmail.com>
2021-02-26 16:53:06 +0000

    card viewer prototype

--- src/ClassSelect.gd
+++ src/ClassSelect.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var ClassButton_load = preload("res://ClassButton.tscn")
+var ClassButton_load = preload("res://src/ClassButton.tscn")
 
 func _ready():
 	
@@ -8,7 +8,7 @@ func _ready():
 	for class_ref in Globals.classes_data:
 		var class_data = Globals.classes_data[class_ref]
 		var button = ClassButton_load.instance()
-		button.text = class_data.name
+		button.text = "  " + class_data.name
 		if class_data.has("icon_path"):
 			button.icon = load(class_data.icon_path)
 		button.disabled = not class_data.has("front_path")
